0e3e15710c2474493fb2bcfa9ba723c7bec8e5aa
fgjsdfgklj <130589053+fgjsdfgklj@users.noreply.github.com>
2023-05-02 14:30:53 -0400

    setting up background

--- A-4/src/App.java
+++ A-4/src/App.java
@@ -1,5 +1,32 @@
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+
 public class App {
+    private JFrame frame;
+    private JLabel backgroundOne;
+    private ImageIcon icon;
+
+    public App() {
+
+        icon = new ImageIcon(this.getClass().getResource("theme park background(1).png"));
+        backgroundOne = new JLabel(icon);
+        backgroundOne.setSize(1920,1080);
+
+        frame = new JFrame();
+        frame.add(backgroundOne);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Balloon Pop");
+        frame.setSize(1920, 1080);
+        frame.setLayout(null);
+        frame.setVisible(true);
+
+
+    }
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        new App();
+
     }
 }
