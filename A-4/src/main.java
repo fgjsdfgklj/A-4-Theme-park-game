ef05ac2ed8c42bef06b6bafec3b2d25f3e6dc432
fgjsdfgklj <130589053+fgjsdfgklj@users.noreply.github.com>
2023-05-05 13:50:43 -0400

    set up crosshair and added comments

--- A-4/src/main.java
+++ A-4/src/main.java
@@ -1,4 +1,7 @@
+import java.awt.Cursor;
 import java.awt.Image;
+import java.awt.Point;
+import java.awt.Toolkit;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -16,10 +19,13 @@ public class main {
         imageIcon = new ImageIcon("theme park background(1).png");
         image = imageIcon.getImage();
         imageIcon = new ImageIcon(image.getScaledInstance(1280, 1024, java.awt.Image.SCALE_SMOOTH));
+        // corsshair image import
+        Image crosshairpng = Toolkit.getDefaultToolkit().getImage("crosshair.png");
+        //setting background
         backgroundOne = new JLabel();
         backgroundOne.setIcon(imageIcon);
         backgroundOne.setLocation(0, 0);
-        backgroundOne.setSize(1920, 1080);
+        backgroundOne.setSize(1280, 1024);
         // adds frame and its values
         frame = new JFrame();
         frame.add(backgroundOne);
@@ -28,7 +34,10 @@ public class main {
         frame.setSize(1920, 1080);
         frame.setLayout(null);
         frame.setVisible(true);
-
+        //set crosshair
+        Point p = new Point(0, 0);
+        Cursor crosshair = Toolkit.getDefaultToolkit().createCustomCursor(crosshairpng, p, "crosshair");
+        backgroundOne.setCursor(crosshair);
     }
 
     public static void main(String[] args) throws Exception {
